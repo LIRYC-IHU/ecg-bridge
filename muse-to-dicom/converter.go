c1559d5da112e6709f6bb2f4d52d4195ce5d0d05
jmilhas <jonathan.milhas@ihu-liryc.fr>
2026-06-04 14:44:45 +0200

    feat: Add MUSE to DICOM converter with stdout support

---
+++ muse-to-dicom/converter.go
@@ -0,0 +1,45 @@
+package musetodicom
+
+import (
+	"fmt"
+	"os"
+
+	musetofda "converter-fda/muse-to-fda"
+
+	"github.com/suyashkumar/dicom"
+)
+
+// Convert parses a GE MUSE RestingECG XML file and writes a DICOM
+// 12-lead ECG waveform file. If outputPath is empty, output goes to stdout.
+func Convert(inputPath, outputPath string) error {
+	data, err := os.ReadFile(inputPath)
+	if err != nil {
+		return fmt.Errorf("reading %s: %w", inputPath, err)
+	}
+
+	d, err := musetofda.ParseMuse(data)
+	if err != nil {
+		return fmt.Errorf("parsing MUSE XML: %w", err)
+	}
+
+	ds, err := BuildDICOM(d)
+	if err != nil {
+		return fmt.Errorf("building DICOM: %w", err)
+	}
+
+	var f *os.File
+	if outputPath == "" {
+		f = os.Stdout
+	} else {
+		f, err = os.Create(outputPath)
+		if err != nil {
+			return fmt.Errorf("creating output file: %w", err)
+		}
+	}
+	defer f.Close()
+
+	if err := dicom.Write(f, ds); err != nil {
+		return fmt.Errorf("writing DICOM: %w", err)
+	}
+	return nil
+}
